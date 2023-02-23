89cd54fef7df860d0cdaf95cc70b28b1ae5c9032
Letícia Santana <lesantanaleticia@gmail.com>
2023-02-23 17:14:14 -0300

    implementado os links da pagina home

--- src/modules/public/pages/home/pages/Home.jsx
+++ src/modules/public/pages/home/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Cabecalho from "../../cabecalho/Cabecalho";
+import {} from "../../../styles/Home.css";
 import {} from '../../../styles/Reset.css'; 
 import MenuLateral from "../../menu-lateral/MenuLateral";
 
@@ -6,7 +7,19 @@ export const Home = () => {
     return(
         <>
             <Cabecalho/>
+            <main className="principal">
             <MenuLateral/>
+        
+                <ul className="principal-links">
+                    <li>Balcão de Vendas</li>
+                    <li>Movimento Financeiro</li>
+                    <li>Cadastro de Produtos</li>
+                    <li>Estoque</li>
+                    <li>Relatórios</li>
+                    <li>Perfil</li>
+                    
+                </ul>
+            </main>
         </>
         
     )
