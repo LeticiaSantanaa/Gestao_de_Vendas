13c43fa047ff5567972fcebb5a9da7fe425c5a9b
Letícia Santana <lesantanaleticia@gmail.com>
2023-02-23 11:47:51 -0300

    Instalado MUI, React-Router-DOM e adicionado Rotas

---
+++ src/modules/public/pages/cabecalho/Cabecalho.jsx
@@ -0,0 +1,15 @@
+import {} from '../../styles/Cabecalho.css'
+import {} from '../../styles/EstiloGlobal.css'
+
+export const Cabecalho = () => {
+    return( 
+       <>
+       <section className="cabecalho">
+        <h1>Gestão de Vendas</h1>
+       </section>
+       
+       </>
+        )
+    
+};
+export default Cabecalho;
---
+++ src/modules/public/pages/home/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Cabecalho from "../../cabecalho/Cabecalho";
+
+export const Home = () => {
+    return(
+        <>
+        <Cabecalho/>
+        <section>
+            <h2>Corpo da pagina</h2>
+        </section>
+
+        </>
+        
+        
+        )
+    
+};
+export default Home;
